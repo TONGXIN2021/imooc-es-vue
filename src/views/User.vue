54fd008d82bf84b424d3408095c277117cf5569d
TONG XIN <tongxin@TONGdeMacBook-Pro.local>
2022-02-22 13:18:22 +0800

    第一次上传文件

---
+++ src/views/User.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div>用户信息</div>
+    <button v-on:click="asc">升序</button>
+    <button @click="desc">降序</button>
+    <button @click="reset">重置</button>
+    <ul>
+      <li v-for="(item, index) in userList" :key="index">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      userList: []
+    }
+  },
+  /*   created () {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        console.log(res)
+        this.userList = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  } */
+
+  /*   async created () {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    console.log(res)
+    this.userList = res.data
+  } */
+
+  async created () {
+    // 解构赋值
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+    console.log(data)
+
+    // 代理
+    this.proxy = new Proxy({}, {
+      get (target, key) {
+        if (key === 'asc') {
+          return [].concat(data).sort((a, b) => a.name > b.name ? 1 : -1)
+        } else if (key === 'desc') {
+          return [].concat(data).sort((a, b) => a.name < b.name ? 1 : -1)
+        } else {
+          return data
+        }
+      },
+      set () {
+        return false
+      }
+    })
+    this.userList = this.proxy.default
+  },
+  methods: {
+    asc () {
+      this.userList = this.proxy.asc
+    },
+    desc () {
+      this.userList = this.proxy.desc
+    },
+    reset () {
+      this.userList = this.proxy.default
+    }
+  }
+}
+</script>
+
+<style>
+li {
+  list-style: none;
+}
+ul{
+  padding: 0;
+}
+</style>
